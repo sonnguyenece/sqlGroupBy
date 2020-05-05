cf2a722dafef7b27d81b5457b59e65dda5364da6
sonnguyenece@gmail.com <sonnguyenece@gmail.com>
2020-05-05 14:18:30 +0700

    add

---
+++ sqlGroup.sql
@@ -0,0 +1,26 @@
+use classicmodels;
+select status from orders
+group by status;
+
+select status, count(*) as 'So luong status' 
+from orders
+group by status
+order by status
+;
+
+select status, sum(quantityOrdered * priceEach) as amount
+from orders 
+inner join orderdetails on orders.ordernumber = orderdetails.ordernumber
+group by status
+order by status;
+
+select orderNumber, sum(quantityOrdered * priceEach) as total
+from orderdetails
+group by orderNumber;
+
+select year(orderDate) as year, sum(quantityOrdered * priceEach) as total
+from orders 
+inner join orderdetails on orders.orderNumber = orderdetails.orderNumber
+where status = 'shipped'
+group by year
+having year > 2003
